cd91a1bf2e2eb46fe2a98669c1b6f15a01dd17f0
BjoernKorioth <bkorioth@gmx.de>
2019-12-15 16:16:10 +0100

    path update

--- MoveIt/src/app/app-routing.module.ts
+++ MoveIt/src/app/app-routing.module.ts
@@ -3,19 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'leaderboard', loadChildren: './leaderboard/leaderboard.module#LeaderboardPageModule' },
-  { path: 'newsfeed', loadChildren: './newsfeed/newsfeed.module#NewsfeedPageModule' },
-  { path: 'rewards', loadChildren: './rewards/rewards.module#RewardsPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
-  { path: 'information', loadChildren: './information/information.module#InformationPageModule' },
-  { path: 'progress', loadChildren: './progress/progress.module#ProgressPageModule' },
-  { path: 'edit-activity', loadChildren: './edit-activity/edit-activity.module#EditActivityPageModule' },
-  { path: 'add-activity', loadChildren: './add-activity/add-activity.module#AddActivityPageModule' },
-  { path: 'edit-goal', loadChildren: './edit-goal/edit-goal.module#EditGoalPageModule' },
-  { path: 'registration', loadChildren: './registration/registration.module#RegistrationPageModule' },
-  { path: 'log-in', loadChildren: './log-in/log-in.module#LogInPageModule' },
-  { path: 'terms-of-use', loadChildren: './terms-of-use/terms-of-use.module#TermsOfUsePageModule' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+  { path: 'leaderboard', loadChildren: './pages/leaderboard/leaderboard.module#LeaderboardPageModule' },
+  { path: 'newsfeed', loadChildren: './pages/newsfeed/newsfeed.module#NewsfeedPageModule' },
+  { path: 'rewards', loadChildren: './pages/rewards/rewards.module#RewardsPageModule' },
+  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  { path: 'information', loadChildren: './pages/information/information.module#InformationPageModule' },
+  { path: 'progress', loadChildren: './pages/progress/progress.module#ProgressPageModule' },
+  { path: 'edit-activity', loadChildren: './pages/edit-activity/edit-activity.module#EditActivityPageModule' },
+  { path: 'add-activity', loadChildren: './pages/add-activity/add-activity.module#AddActivityPageModule' },
+  { path: 'edit-goal', loadChildren: './pages/edit-goal/edit-goal.module#EditGoalPageModule' },
+  { path: 'registration', loadChildren: './pages/registration/registration.module#RegistrationPageModule' },
+  { path: 'log-in', loadChildren: './pages/log-in/log-in.module#LogInPageModule' },
+  { path: 'terms-of-use', loadChildren: './pages/terms-of-use/terms-of-use.module#TermsOfUsePageModule' },
 ];
 
 @NgModule({
