561ec8c93ca5467b0cdb7e6ff49bdbb13a05bed7
crystalnet <ko.schmidt@posteo.de>
2020-01-01 22:10:00 +0100

    added documentation

--- MoveIt/src/app/model/user.ts
+++ MoveIt/src/app/model/user.ts
@@ -7,8 +7,15 @@ export class User {
     trophiesWon: Array<any>;
     type: string;
 
+    /**
+     * Constructor to create User object
+     *
+     * Each parameter is optinal. If it's not present, a default value is used
+     *
+     */
     constructor(id?: string, name?: string, challengesActive?: Array<any>, challengesWon?: Array<any>, group?: number,
                 trophiesWon?: Array<any>, type?: string) {
+        // Each parameter is optional, if it's not there, set the default value
         this.id = id || '';
         this.name = name || '';
         this.challengesActive = challengesActive || [];
@@ -18,7 +25,17 @@ export class User {
         this.type = type || 'user';
     }
 
+    /**
+     * toFireBaseObject
+     *
+     * Converts the User object to a firebase object. It basically just replaces the arrays with a JSON string.
+     * We could also hand over the whole user object to firebase, but then it would use the arrays as subelements.
+     * If we substitute the arrays with their string representation, firebase will just store them as a string as well
+     * and not try to parse them.
+     *
+     */
     toFirebaseObject() {
+
         return {
             name: this.name,
             challengesActive: JSON.stringify(this.challengesActive),
