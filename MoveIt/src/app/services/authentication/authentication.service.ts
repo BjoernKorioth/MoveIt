e6a72e7b37adf3409c91fd66f1a4bd6607087c36
crystalnet <ko.schmidt@posteo.de>
2020-01-01 22:01:08 +0100

    fully working registration process; reformatted files

--- MoveIt/src/app/services/authentication/authentication.service.ts
+++ MoveIt/src/app/services/authentication/authentication.service.ts
@@ -1,66 +1,68 @@
-import { Injectable } from "@angular/core";
+import {Injectable} from '@angular/core';
 import * as firebase from 'firebase/app';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
 import {User} from '../../model/user';
-
 
 
 @Injectable()
 export class AuthenticateService {
 
-  private user: User;
+    private user: User;
 
-  private list : AngularFireList<any>;
+    private list: AngularFireList<any>;
 
-  constructor(private fireDatabase: AngularFireDatabase){}
+    constructor(private fireDatabase: AngularFireDatabase) {
+    }
 
-  registerUser(value){
-   return new Promise<any>((resolve, reject) => {
-     firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
-     .then(  
-       res => resolve(res),
-       err => reject(err),)
-   })
-  }
-
-   registerOnDatabase(value){
-      let record =  {uid: firebase.auth().currentUser.uid , challengesActive:"{}", challengesWon:"{}", group: 2, name: value.surname+", " +value.firstname, trophiesWon:"{}", type:"user" };
-      console.log(record);
-      console.log("Test "+ this.fireDatabase.list("/users"));
-      
-      this.fireDatabase.database.ref().child('users').child(firebase.auth().currentUser.uid).set(record);
-      
-  }
-
-  loginUser(value){
-   return new Promise<any>((resolve, reject) => {
-     firebase.auth().signInWithEmailAndPassword(value.email, value.password)
-     .then(
-       res => resolve(res),
-       err => reject(err))
-   })
-  }
-
-  logoutUser(){
-    return new Promise((resolve, reject) => {
-      if(firebase.auth().currentUser){
-        firebase.auth().signOut()
-        .then(() => {
-          console.log("LOG Out");
-          resolve();
-        }).catch((error) => {
-          reject();
+    registerUser(value) {
+        return new Promise<any>((resolve, reject) => {
+            firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+                .then(
+                    (userCredential) => {
+                        const user = userCredential.user;
+                        this.user = new User(user.uid, value.firstname + ' ' + value.surname);
+                        this.registerOnDatabase().then(
+                            res => resolve(res),
+                            err => reject(err)
+                        );
+                    },
+                    err => reject(err));
         });
-      }
-    })
-  }
+    }
 
-  userDetails(){
-    return firebase.auth().currentUser;
-  }
+    registerOnDatabase() {
+        return this.fireDatabase.database.ref().child('users').child(this.user.id).set(this.user.toFirebaseObject());
+    }
 
-  loggedUserDetails(){
-    return this.user;
-  }
+    loginUser(value) {
+        return new Promise<any>((resolve, reject) => {
+            firebase.auth().signInWithEmailAndPassword(value.email, value.password)
+                .then(
+                    res => resolve(res),
+                    err => reject(err));
+        });
+    }
+
+    logoutUser() {
+        return new Promise((resolve, reject) => {
+            if (firebase.auth().currentUser) {
+                firebase.auth().signOut()
+                    .then(() => {
+                        console.log('LOG Out');
+                        resolve();
+                    }).catch((error) => {
+                    reject(error);
+                });
+            }
+        });
+    }
+
+    userDetails() {
+        return firebase.auth().currentUser;
+    }
+
+    loggedUserDetails() {
+        return this.user;
+    }
 }
- 
+
