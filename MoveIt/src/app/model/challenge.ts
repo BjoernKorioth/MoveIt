d8f49f9d3943eac3ef69294dd885f5cb4aa2ac3a
Murat Ugur Selcuk <mselcuk@mail.uni-mannheim.de>
2020-01-08 23:52:57 +0100

    implemented challenges get from DB, join or disjoin to challenge

---
+++ MoveIt/src/app/model/challenge.ts
@@ -0,0 +1,72 @@
+interface FireBaseObject {
+    id: string;
+    description: string;
+    endTime: string;
+    price: string;
+    startTime: string;
+    title: string;
+}
+
+export class Challenge{
+
+    /**
+     * Constructor to create Activity
+     *
+     * Each parameter is optional. If it's not present, a default value is used
+     *
+     */
+    constructor(id?: string, description?: string, endTime?: Date, price?: string, startTime?: Date, title?: string) {
+        // Each parameter is optional, if it's not there, set the default value
+        this.id = id || '';
+        this.description = description || 'You must walk 10km a day for 1 week';
+        this.endTime = endTime || new Date(2019, 0O5, 0O5, 17, 23, 42, 0);
+        this.price = price || '10$ amazon gift card';
+        this.startTime = startTime || new Date(2019, 0O5, 0O5, 17, 55, 42, 0);
+        this.title = title || 'running';
+    }
+
+    static types = ['running', 'swimming', 'workout'];
+    static intensities = ['moderate', 'vigorous', 'weight training'];
+    id: string;
+    description: string;
+    endTime: Date;
+    price: string;
+    startTime: Date;
+    title: string;
+
+    /**
+     * Creates an Activity object from a firebase query
+     *
+     * This basically reconstructs the dates from the date strings
+     *
+     * @param id id of the activity
+     * @param firebaseObject result of the query
+     */
+
+    static fromFirebaseObject(id: string, firebaseObject: FireBaseObject) {
+        // @ts-ignore TS2339
+        return new Activity(
+            id || '',
+            firebaseObject.description || '',
+            new Date(firebaseObject.endTime) || new Date(),
+            firebaseObject.price || '',
+            new Date(firebaseObject.startTime) || new Date(),
+            firebaseObject.title || ''
+        );
+    }
+
+    /**
+     * Converts the activity to upload it to firebase
+     *
+     * Basically just replaces the dates with date strings
+     */
+    toFirebaseObject() {
+        return {
+            description: this.description,
+            endTime: this.endTime.toDateString(),
+            price: this.price,
+            startTime: this.startTime.toDateString(),
+            title: this.title
+        };
+    }
+}
--- MoveIt/src/app/pages/rewards-detail/rewards-detail.page.ts
+++ MoveIt/src/app/pages/rewards-detail/rewards-detail.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Challenge} from '../../model/challenge';
+
+import {ChallengeService} from '../../services/challenges/challenge.service';
+
+import {Observable} from 'rxjs';
+
 @Component({
   selector: 'app-rewards-detail',
   templateUrl: './rewards-detail.page.html',
@@ -7,10 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RewardsDetailPage implements OnInit {
   trophies: any;
-  challenges: any;
-  activeChallenges: any;
+  challenges: Array<Challenge>;
+  challengesObserve: Observable<Array<Challenge>>;
+  challengesActiveObserve: Observable<Array<Challenge>>
+  activeChallenges: Array<Challenge>;
 
-  constructor() { 
+  constructor(private challService: ChallengeService) { 
+
+    this.challengesObserve = this.challService.getAllAvailableChallenges();
+
+    this.challengesObserve.subscribe(result => this.updateAllChallenges(result));
+
+    this.challengesActiveObserve = this.challService.getAllUserActiveChallenges();
+
+    this.challengesActiveObserve.subscribe(result => {this.updateAllActiveChallenges(result);
+           for(var i = 0; i< this.activeChallenges.length; i++){
+      this.identifyChallenge(this.activeChallenges[i]);
+    }});
+
+
+
     this.trophies = [
       {
         description: 'You get this trophy for winning 10 times a daily goal.',
@@ -28,7 +50,7 @@ export class RewardsDetailPage implements OnInit {
         image: './assets/Trophy.png'
       }
     ]
-    this.challenges= [
+    /*this.challenges= [
       {
         description: 'Run the equivalent of a marathon during one week',
         title: 'Run 100 km within a week',
@@ -43,8 +65,10 @@ export class RewardsDetailPage implements OnInit {
         endDate: 55,
         price: '30 Euro Amazon Gift Card'
       },
-    ]
-    this.activeChallenges= [
+    ]*/
+
+
+    /*this.activeChallenges= [
       {
         description: 'Run the equivalent of a marathon during one week',
         title: 'Run 50 km within a week',
@@ -59,10 +83,50 @@ export class RewardsDetailPage implements OnInit {
         endDate: 55,
         price: '30 Euro Amazon Gift Card'
       }
-    ]
+    ]*/
   }
 
   ngOnInit() {
+
+  }
+
+  updateAllChallenges(newChallenges: Array<Challenge>){
+    this.challenges = newChallenges;
+  }
+
+  updateAllActiveChallenges(newActive: Array<Challenge>){
+    this.activeChallenges = newActive;
+  }
+
+
+  addToActiveList(challenge:Challenge){
+    this.activeChallenges.push(challenge);
+    this.identifyChallenge(challenge);
+    this.challService.addChallengeToActive(this.activeChallenges);
+  }
+
+  removeFromActiveList(activeChallenge:Challenge){
+    this.challenges.push(activeChallenge);
+    this.identifyActiveChallenge(activeChallenge);
+    this.challService.addChallengeToActive(this.activeChallenges);
+  }
+
+  identifyChallenge(challenge:Challenge){
+    console.log(challenge);
+    for(var i = 0; i<this.challenges.length; i++){
+      if(this.challenges[i].title === challenge.title){
+        this.challenges.splice(i,i+1);
+      }
+    }
+  }
+
+  identifyActiveChallenge(challenge:Challenge){
+    console.log(challenge);
+    for(var i = 0; i<this.activeChallenges.length; i++){
+      if(this.activeChallenges[i].title === challenge.title){
+        this.activeChallenges.splice(i,i+1);
+      }
+    }
   }
 
 }
