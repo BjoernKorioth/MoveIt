9ff57c031a66870542662f49e4546c3951a59bb0
cennet10 <38178209+cennet10@users.noreply.github.com>
2020-01-11 03:07:18 +0100

    segments

--- MoveIt/src/app/pages/leaderboard-detail/leaderboard-detail.page.ts
+++ MoveIt/src/app/pages/leaderboard-detail/leaderboard-detail.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderboardDetailPage implements OnInit {
   persons: any;
+  ranking: string = "actMinutes";
+
   constructor() {
   this.persons = [
     {
