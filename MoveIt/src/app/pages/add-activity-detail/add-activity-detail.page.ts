a8378cbfe5007ecd87c50797331df9abd389da81
cennet10 <38178209+cennet10@users.noreply.github.com>
2020-01-13 07:12:50 +0100

    First try for stopwatch

--- MoveIt/src/app/pages/add-activity-detail/add-activity-detail.page.ts
+++ MoveIt/src/app/pages/add-activity-detail/add-activity-detail.page.ts
@@ -13,6 +13,21 @@ export class AddActivityDetailPage implements OnInit {
   minutes: number;
   types: Array<string>;
   intensities: Array<string>;
+ // percent:number = 0;
+ // radius:number = 100;
+
+  //fullTime: any = '00:01:30';
+  //timer: any = false;
+  //progress: any = false;
+  //minutes2:number = 1;
+  //seconds: any = 30;
+  elapsed:any = {
+    h: '00',
+    m: '00',
+    s: '00'
+  }
+  overallTimer: any = false;
+
 
   constructor(private activityService: ActivityService, private location: Location) {
     this.activity = new Activity();
@@ -37,5 +52,72 @@ export class AddActivityDetailPage implements OnInit {
         err => console.log(err)
     );
   }
+
+  startTimer(){
+
+   /* if(this.timer){
+      clearInterval(this.timer);
+    }*/
+    if(!this.overallTimer){
+      this.progressTimer();
+    }
+
+    /*this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+
+    let timeSplit = this.fullTime.split(':');
+    this.minutes2 = timeSplit[1];
+    this.seconds = timeSplit[2];
+
+    let totalSeconds = Math.floor(this.minutes2 * 60) + parseInt(this.seconds);
+    
+    this.timer = setInterval(() => {
+     if(this.percent == this.radius){
+        clearInterval(this.timer);
+      }
+     // this.percent = Math.floor((this.progress / totalSeconds) * 100);
+      this.progress++;
+    }, 1000)*/
+  }
+
+  progressTimer(){
+    let countDownDate = new Date();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / (1000));
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+
+    }, 1000)
+  }
+
+  pad(num, size) {
+    let s = num+"";
+    while(s.length < size) s = "0" + s;
+    return s;
+  }
+
+  stopTimer(){
+    //clearInterval(this.timer);
+    clearInterval(this.overallTimer);
+    this.overallTimer = false;
+    //this.timer = false;
+    //this.percent = 0;
+    //this.progress = 0;
+    this.elapsed = {
+      h:'00',
+      m:'00',
+      s:'00'
+    }
+  }
+
 
 }
