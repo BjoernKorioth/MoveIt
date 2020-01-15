2652dc79441ff467a4b93e3506d4fcda0543f5d7
taib06 <taib.demirci@hotmail.de>
2020-01-15 13:43:23 +0100

    id-Übergabe geklärt

--- MoveIt/src/app/pages/socialfeed-detail/socialfeed-detail.page.ts
+++ MoveIt/src/app/pages/socialfeed-detail/socialfeed-detail.page.ts
@@ -98,13 +98,19 @@ export class SocialfeedDetailPage implements OnInit {
   }
 
   newComment() {
-    var cUser = document.getElementsByTagName("input")[0].value;
-    this.comments.push({ userid : '4' , name : 'Greg' ,comment: cUser});
-    
+    var userId = document.getElementsByClassName("idPlace")[0].id;
+    var userComment = document.getElementsByTagName("input")[0].value;
+
+    this.comments.push({ userid : '4' , name : userId ,comment: userComment});
     this.postService.createComment('-LxfARsp_2al7-W3JYcf', new Comment()).then(
         res => console.log(res),
         err => console.log(err)
     );
+  }
+
+  myFunction(i){
+    var xxx = document.getElementsByName("userPlace")[i].id;
+    document.getElementsByName("demo")[i].innerHTML = xxx;
   }
 
   editComment() {
