4ee9a776a9a85461fb8fff0c0da8ba271059599d
cennet10 <38178209+cennet10@users.noreply.github.com>
2020-01-24 10:15:39 +0100

    working back button in goals

--- MoveIt/src/app/pages/goals-detail/goals-detail.page.ts
+++ MoveIt/src/app/pages/goals-detail/goals-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from  '@angular/common';
 
 @Component({
   selector: 'app-goals-detail',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
   }
+
+  goBack(){
+    this.location.back();
+  }
+  
 
 }
