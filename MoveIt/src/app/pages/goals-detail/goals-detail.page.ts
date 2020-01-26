3d699d40a48f5cb9e3037bd065f9e48d005a645b
taib06 <taib.demirci@hotmail.de>
2020-01-26 16:04:32 +0100

    GoalsController

--- MoveIt/src/app/pages/goals-detail/goals-detail.page.ts
+++ MoveIt/src/app/pages/goals-detail/goals-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GoalService} from '../../services/goal/goal.service';
 
 @Component({
   selector: 'app-goals-detail',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsDetailPage implements OnInit {
 
-  constructor() { }
+  goals:any;
+
+  constructor(private goalService: GoalService) {
+    this.goals = this.goalService.getGoals();
+    
+  }
 
   ngOnInit() {
+  }
+
+  moderate(){
+    this.goalService.getGoal('dailyModerate').then(
+      res => {
+          document.getElementById("daily").setAttribute("value",res.target);
+          document.getElementById("daily").setAttribute("name","dailyModerate");
+      },
+      err => console.log(err)
+    );
+
+    this.goalService.getGoal('weeklyModerate').then(
+      res => {
+          document.getElementById("weekly").setAttribute("value",res.target);
+          document.getElementById("weekly").setAttribute("name","weeklyModerate");
+      },
+      err => console.log(err)
+    );
+  }
+
+  vigorous(){
+    this.goalService.getGoal('dailyVigorous').then(
+      res => {
+        document.getElementById("daily").setAttribute("value",res.target);
+        document.getElementById("daily").setAttribute("name","dailyVigorous");
+      },
+      err => console.log(err)
+    );
+
+    this.goalService.getGoal('weeklyVigorous').then(
+      res => {
+          document.getElementById("weekly").setAttribute("value",res.target);
+          document.getElementById("weekly").setAttribute("name","weeklyVigorous");
+      },
+      err => console.log(err)
+    );
+  }
+
+  weight(){
+    this.goalService.getGoal('dailyWeight').then(
+      res => {
+          document.getElementById("daily").setAttribute("value",res.target);
+          document.getElementById("daily").setAttribute("name","dailyWeight");
+      },
+      err => console.log(err)
+    );
+
+    this.goalService.getGoal('weeklyWeight').then(
+      res => {
+          document.getElementById("weekly").setAttribute("value",res.target);
+          document.getElementById("weekly").setAttribute("name","weeklyWeight");
+      },
+      err => console.log(err)
+    );
+  }
+
+  adjustGoal(){
+    var changedDaily = document.getElementById("daily").getAttribute("name");
+    var changedWeekly = document.getElementById("weekly").getAttribute("name");
+
+    this.goalService.getGoal(changedDaily).then(
+      res => {
+        console.log(this.goalService.adjustGoal(res,Number(document.getElementsByTagName("input")[0].value)));
+      },
+      err => console.log(err)
+    );
+
+    this.goalService.getGoal(changedWeekly).then(
+      res => {
+        console.log(this.goalService.adjustGoal(res,Number(document.getElementsByTagName("input")[1].value)));
+      },
+      err => console.log(err)
+    );
   }
 
 }
