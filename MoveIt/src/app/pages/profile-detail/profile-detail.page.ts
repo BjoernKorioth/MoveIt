51523784ae0646f113a8898759c37eebf25cab8d
klaraala <56079562+klaraala@users.noreply.github.com>
2020-01-26 23:14:39 +0100

    hinzufügen der goals und activities

--- MoveIt/src/app/pages/profile-detail/profile-detail.page.ts
+++ MoveIt/src/app/pages/profile-detail/profile-detail.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from  '@angular/common';
+import { GoalService } from 'src/app/services/goal/goal.service';
+import { Activity } from 'src/app/model/activity';
+import { ActivityService } from 'src/app/services/activity/activity.service';
+import {Goal} from '../../model/goal';
+import {Observable} from 'rxjs';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -8,8 +14,19 @@ import { Location } from  '@angular/common';
   styleUrls: ['./profile-detail.page.scss'],
 })
 export class ProfileDetailPage implements OnInit {
+  activities: Observable<Activity[]>;
+  goals: Observable<any>;
+  goalStorage: Array<Goal>;
 
-  constructor(private location:Location) { }
+  constructor(private location:Location, private goalService: GoalService, private activityService: ActivityService) { 
+    this.activities = this.activityService.getAllUserActivities();
+    this.goals = this.goalService.getGoals();
+    this.goals.subscribe(goals => this.goalStorage = goals);
+    //this.router = router;
+  }
+
+
+  
 
   ngOnInit() {
   }
