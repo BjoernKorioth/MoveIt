e866f16cd7a59848a7c86840ff1e5472245ee190
crystalnet <ko.schmidt@posteo.de>
2020-02-03 02:45:24 +0100

    updated git bash script

--- updateBranches.sh
+++ updateBranches.sh
@@ -33,3 +33,19 @@ git push origin rewards;
 git checkout -B dashboard --track origin/dashboard;
 git merge --ff-only origin/master;
 git push origin dashboard;
+
+git checkout -B dashboard --track origin/profile;
+git merge --ff-only origin/master;
+git push origin profile;
+
+git checkout -B dashboard --track origin/tracking;
+git merge --ff-only origin/master;
+git push origin tracking;
+
+git checkout -B dashboard --track origin/usergroups;
+git merge --ff-only origin/master;
+git push origin usergroups;
+
+git checkout -B dashboard --track origin/goals;
+git merge --ff-only origin/master;
+git push origin goals;
