8d217164211b9d1513b9c9b795e49025aeab8004
cennet10 <38178209+cennet10@users.noreply.github.com>
2020-02-07 21:11:35 +0100

    validate add activity

--- MoveIt/src/app/pages/add-activity-manual/add-activity-manual.module.ts
+++ MoveIt/src/app/pages/add-activity-manual/add-activity-manual.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -24,6 +24,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes),
     NgCircleProgressModule.forRoot({
       // set defaults here
