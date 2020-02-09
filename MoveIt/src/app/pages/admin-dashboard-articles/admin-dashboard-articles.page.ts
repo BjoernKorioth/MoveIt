b006f9e46879ec8a2fcd3f303b8d557b2dff1fc0
klaraala <56079562+klaraala@users.noreply.github.com>
2020-02-09 19:00:58 +0100

    Bild Upload für Information

--- MoveIt/src/app/pages/admin-dashboard-articles/admin-dashboard-articles.page.ts
+++ MoveIt/src/app/pages/admin-dashboard-articles/admin-dashboard-articles.page.ts
@@ -1,11 +1,24 @@
 import {Component, OnInit} from '@angular/core';
 import {Information} from '../../model/information';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {InformationService} from '../../services/information/information.service';
+import {AlertController} from '@ionic/angular';
+import {AngularFireStorage, AngularFireUploadTask} from '@angular/fire/storage';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {finalize, tap, catchError} from 'rxjs/operators';
+import * as firebase from 'firebase';
 
 
 import {PopoverController} from '@ionic/angular';
 import {ArticlesPopoverComponent} from 'src/app/articles-popover/articles-popover.component';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+export interface MyData {
+    name: string;
+    filepath: string;
+    size: number;
+}
+
 
 @Component({
     selector: 'app-admin-dashboard-articles',
@@ -16,12 +29,45 @@ export class AdminDashboardArticlesPage implements OnInit {
     articlesObserve: Observable<Information[]>;
     articles: Array<Information>;
     information: Information;
+    private imageCollection: AngularFirestoreCollection<MyData>;
+ 
+    infoID: String;
 
-
-    constructor(private informationService: InformationService, public popoverController: PopoverController) {
+    constructor(private storage: AngularFireStorage, private database: AngularFirestore, private db: AngularFireDatabase, private informationService: InformationService, public popoverController: PopoverController) {
         this.articlesObserve = this.informationService.getAllInformation();
         this.articlesObserve.subscribe(result => this.updateAllInformation(result));
+        this.isUploading = false;
+        this.isUploaded = false;
+
+         // Set collection where our documents/ images info will save
+         this.imageCollection = database.collection<MyData>('profilePic');
+         this.images = this.imageCollection.valueChanges();
+        
     }
+
+    // Upload Task
+    task: AngularFireUploadTask;
+
+// Progress in percentage
+    percentage: Observable<number>;
+
+// Snapshot of uploading file
+    snapshot: Observable<any>;
+
+// Uploaded File URL
+    UploadedFileURL: Observable<string>;
+
+// Uploaded Image List
+    images: Observable<MyData[]>;
+
+// File details
+    fileName: string;
+    fileSize: number;
+
+// Status check
+    isUploading: boolean;
+    isUploaded: boolean;
+
 
     editArticle(article: Information) {
         console.log(article);
@@ -42,6 +88,66 @@ export class AdminDashboardArticlesPage implements OnInit {
         });
         return await popover.present();
     }
+
+    uploadFile(event: FileList, id: string) {
+
+
+        // The File object
+        const file = event.item(0);
+
+        // Validation for Images Only
+        if (file.type.split('/')[0] !== 'image') {
+            console.error('unsupported file type :( ');
+            return;
+        }
+
+        this.isUploading = true;
+        this.isUploaded = false;
+
+
+        this.fileName = file.name;
+
+        // The storage path
+        const path = `AdminDashboard/${this.fileName}`;
+
+        // File reference
+        const fileRef = this.storage.ref(path);
+        // The main task
+        this.task = this.storage.upload(path, file);
+        // Get file progress percentage
+        this.percentage = this.task.percentageChanges();
+        this.snapshot = this.task.snapshotChanges().pipe(
+           
+            finalize(() => {
+                // Get uploaded file storage path
+                this.UploadedFileURL = fileRef.getDownloadURL();
+
+                this.UploadedFileURL.subscribe(resp => {
+                    return this.db.database.ref('/information/' + id + '/picture').set(resp).then(
+                        res => console.log(res),
+                        err => console.log(err)
+                    );
+                    
+                }, error => {
+                    console.error(error);
+                });
+            }),
+            tap(snap => {
+                this.fileSize = snap.totalBytes;
+                console.log(snap);
+            }),
+            catchError(error => of(error))
+        );
+        this.snapshot.subscribe(val => console.log(val));
+    }
+
+    addPictureToInfo(pictureUrl: string, infoID: string) {
+        return this.db.database.ref('/information/' + infoID + '/picture').set(pictureUrl);
+   
+       
+    }
+
+
 
     ngOnInit() {
     }
