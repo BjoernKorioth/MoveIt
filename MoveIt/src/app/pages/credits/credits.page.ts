b4034c18f704fddb64da3b9230ae292fbe8afb05
cennet10 <38178209+cennet10@users.noreply.github.com>
2020-02-11 16:53:23 +0100

    credits page created

---
+++ MoveIt/src/app/pages/credits/credits.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-credits',
+  templateUrl: './credits.page.html',
+  styleUrls: ['./credits.page.scss'],
+})
+export class CreditsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
