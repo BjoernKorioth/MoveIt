58b949477072271cd277d75a49a08311c2dc42dd
BjoernKorioth <bkorioth@gmx.de>
2020-02-12 16:48:50 +0100

    Firebase Cloud Messaging

--- MoveIt/src/app/app.component.ts
+++ MoveIt/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { FCM } from '@ionic-native/fcm/ngx';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -17,6 +20,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private fcm: FCM,
+    private router: Router,
   ) {
     this.initializeApp();
   }
@@ -25,6 +30,25 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.fcm.getToken().then(token => {
+        console.log(token);
+      });
+      this.fcm.onTokenRefresh().subscribe(token => {
+        console.log(token);
+      });
+      this.fcm.onNotification().subscribe(data => {
+        console.log(data);
+        if (data.wasTapped) {
+          console.log('Received in background');
+          //this.router.navigate([data.landing_page, data.price]);
+          this.router.navigate(['/menu/dashboard']);
+        } else {
+          console.log('Received in foreground');
+          //this.router.navigate([data.landing_page, data.price]);
+          this.router.navigate(['/menu/dashboard']);
+        }
+        });
+          
     });
   }
 
