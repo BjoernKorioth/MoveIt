86f9f08d771fb3c0a3e3772007a03b1a045ea67d
crystalnet <ko.schmidt@posteo.de>
2020-09-09 15:27:45 +0200

    show toast after synchronizing activities

--- MoveIt/src/app/pages/add-overview/add-overview.page.ts
+++ MoveIt/src/app/pages/add-overview/add-overview.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
+import { ToastController} from '@ionic/angular';
 import {ActivityService} from '../../services/activity/activity.service';
 
 @Component({
@@ -10,7 +11,7 @@ import {ActivityService} from '../../services/activity/activity.service';
 export class AddOverviewPage implements OnInit {
     lastDate: Date;
 
-    constructor(private location: Location, private activityService: ActivityService) {
+    constructor(private location: Location, private activityService: ActivityService, private toastController: ToastController) {
         activityService.getLastDate().then(
             res => this.lastDate = res,
             err => console.log(err));
@@ -24,9 +25,29 @@ export class AddOverviewPage implements OnInit {
     }
 
     synchronizeActivities() {
-      this.activityService.synchronizeApi().then(
-          res => console.log(res),
-          err => console.log(err)
-      );
+        this.activityService.synchronizeApi().then(
+            res => {
+                console.log(res);
+                this.toastController.create({
+                    color: 'dark',
+                    duration: 2000,
+                    message: 'Activities successfully synchronized',
+                    showCloseButton: true
+                }).then(toast => {
+                    toast.present();
+                });
+            },
+            err => {
+                console.log(err);
+                this.toastController.create({
+                    color: 'dark',
+                    duration: 2000,
+                    message: 'There was an error synrhconizing the activities: ' + err,
+                    showCloseButton: true
+                }).then(toast => {
+                    toast.present();
+                });
+            }
+        );
     }
 }
